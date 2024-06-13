8c6066ddce989422da454feeb0cbdc7e25592251
Gustavo Viegas <gustavoprado062006@gmail.com>
2024-06-13 08:21:41 -0300

    Inicio da classe usuários

---
+++ Modulos/Usuario.py
@@ -0,0 +1,58 @@
+class Usuario:
+  """ Documentação da Classe no Python
+      Funções do usuário padrão da Lanchonete.
+  """
+
+  # propriedades da classe (variáveis)
+  # cadeia - string - str - "texto 1234" "1234"
+  loginInformado:str = "cliente"
+  senhaInformada:str = "1234"
+
+  # Array - matriz ou vetor nomeado
+  dadosUsuario = {
+      "loginArmazenado" : "cliente",
+      "senhaArmazenada" : "1234",
+      "nomeUsuario" : "João dos Santos",
+  }
+
+
+  # Método Construtor: executado ao instanciar a classe
+  # self refere-se à instância da classe
+  def __init__( self ):
+
+    # chamando o método logar da classe
+    self.logar()
+
+  def logar ( self ):
+
+    self.loginInformado =  input( "Informe o login: \n" )
+    self.senhaInformada =  input( "Informe a senha: \n" )
+
+    # Comparação - Condicionais - Se - if
+    # senão - else - falso
+
+    if self.loginInformado == self.dadosUsuario["loginArmazenado"]:
+      print( "Bem Vindo ao Sistema! \n \n" )
+      self.mostraMensagens ("Bem vindo ao sistema!")
+      self.exibirInfosUsuario()
+    else:
+      print( " Falha ao se conectar, tente novamente ")
+
+  def sair( self ):
+    print( "Logout do sistema" )
+
+  def exibirInfosUsuario( self ):
+    print( " Os dados do usuário são: \n Nome: \n Login: " )
+    
+  def mostraMensagens( self, mensagem):
+    print(f"-------------------- \n")
+    print(f"| {mensagem}      | \n")
+    print(f"-------------------- \n")
+
+# Uma classe convencional precisa ser Instanciada para que seus objetos possam ser usados.
+# Instanciar uma classe é colocar uma cópia ( instância ) em uma variável ( objeto )
+# operador de atribuição =
+# atendente = Usuario()
+
+# usando um dos métodos
+# atendente.sair()
---
+++ Sistema.py
@@ -0,0 +1,8 @@
+"""
+Este arquivo é a entrada do sistema, ele aciona os módulos necessários.
+"""
+
+#importação módulo usuário e da classe 
+from Modulos.Usuario import Usuario
+
+usuario = Usuario() 
